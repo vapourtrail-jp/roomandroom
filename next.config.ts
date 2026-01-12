a68aa8475413ec7a5cca1f30856b5201101fc407
hiroshige matsuda <hiroshige@HiroshigenoMacBook-Pro.local>
2026-01-12 16:36:22 +0900

    Add redirect rule

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,22 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   transpilePackages: ['three'],
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'roomandroom.pages.dev',
+          },
+        ],
+        destination: 'https://www.roomandroom.org/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
