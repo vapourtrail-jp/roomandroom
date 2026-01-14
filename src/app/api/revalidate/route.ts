90db8dd7a6ef8cf1c20a9a31db201563b4e88583
hiroshige matsuda <hiroshige@HiroshigenoMacBook-Pro.local>
2026-01-14 15:17:07 +0900

    fix: set api runtime to edge

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
 
+export const runtime = 'edge';
+
 export async function GET(request: NextRequest) {
     const secret = request.nextUrl.searchParams.get('secret');
 
