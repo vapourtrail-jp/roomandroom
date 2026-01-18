df14bc50983b686456dcb0cf19dea405fa00dbf7
hiroshige matsuda <hiroshige@HiroshigenoMacBook-Pro.local>
2026-01-18 22:02:12 +0900

    Restructure home page with NEWS title and home-container styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <Link href="/rooms" style={{ textDecoration: 'none' }}>
-      <div className="home-notice">2026.1.18　room and room. is back!(This is a beta release. click here!)</div>
-    </Link>
+    <div className="home-container">
+      <div className="title">NEWS</div>
+      <div className="content">
+        <Link href="/rooms" style={{ textDecoration: 'none' }}>
+          <div className="home-notice">2026.1.18　room and room. is back!(This is a beta release. click here!)</div>
+        </Link>
+      </div>
+    </div>
   );
 }
